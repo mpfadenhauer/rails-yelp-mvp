05c9fc29b1fcff1664fb59d470fe3f2ca1275d3e
(Maximilian Pfadenhauer) <max.pfadenhauer@gmail.com>
2019-06-18 11:56:29 +0300

    2.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  Restaurant.create(name: "Dudu", address: "Berlin", phone_number: 131313131, category: "french")
+  Restaurant.create(name: "Onza", address: "Tel Aviv", phone_number: 1784184719, category: "belgian")
+  Restaurant.create(name: "Vapiano", address: "Munich", phone_number: 194814981, category: "italian")
+  Restaurant.create(name: "Sababa", address: "Berlin", phone_number: 79714681764, category: "french")
+  Restaurant.create(name: "Cookies", address: "Berlin", phone_number: 901874141, category: "italian")
+
+
